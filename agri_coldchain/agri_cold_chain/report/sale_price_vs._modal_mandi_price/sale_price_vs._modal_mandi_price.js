f8041a4c381780c1bf85ad87956be98b62c4e15c
Dasharath <dashrathchoudhary46@gmail.com>
2026-07-31 11:20:24 +0530

    update:final log files

---
+++ agri_coldchain/agri_cold_chain/report/sale_price_vs._modal_mandi_price/sale_price_vs._modal_mandi_price.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Your Organisation and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sale Price vs. Modal Mandi Price"] = {
+	"filters": [
+
+	]
+};
---
+++ agri_coldchain/agri_cold_chain/report/supplier_grading_&_settlement/supplier_grading_&_settlement.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Your Organisation and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Supplier Grading & Settlement"] = {
+	"filters": [
+
+	]
+};
